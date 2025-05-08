f0e7eb0c08d72f5d6b7812538c68945941fe5af0
nagesh <chintadanagesh9@gmail.com>
2025-05-08 13:20:20 +0530

    some changes are made

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,48 +1,42 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import dotenv from 'dotenv';
-
-import cookieParser from 'cookie-parser'
-
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import { connectDB } from './lib/db.js';
-import cors from 'cors'
-import { server,app } from './lib/socket.js';
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
 
-dotenv.config()
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
+dotenv.config();
 
-
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cookieParser())
-app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
-}))
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use('/api/auth', authRoutes)
-app.use('/api/messages', messageRoutes)
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-  }
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-server.listen(5001, () => {
-    console.log(`Server is running on PORT:`, PORT)
-    connectDB();
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-
-
-export default app
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
